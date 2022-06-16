1478ebe5d1f554defa7b643a9bbf056f388e7b65
Jason Louie <jason.jw.louie@gmail.com>
2022-06-16 11:15:32 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//simple game created using SFML
+//learn more at https://github.com/chubkey3/SFML-Game
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "first commit" << endl;
+
+	return 0;
+}
